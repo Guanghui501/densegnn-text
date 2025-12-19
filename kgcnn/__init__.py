998c599e2ca510f1d513be1544ac122c0bfbd8bc
Guanghui501 <zhanggh6@gmail.com>
2025-12-19 09:41:27 +0800

    Add files via upload

---
+++ kgcnn/__init__.py
@@ -0,0 +1,9 @@
+"""
+kgcnn: Keras Graph Convolutional Neural Networks
+A Keras-based library for graph neural networks.
+"""
+__version__ = "3.1.0"
+__kgcnn_version__ = "3.1.0"
+__indices_axis__ = -2
+__index_receive__ = 0
+__index_send__ = 1
